217ba67d01fea119ba97fdc3e74f66ac44283e9a
Vitoria Garcia <vitoria_garcia_costa_m-tsapp1ma2223@cev.com>
2023-11-02 14:04:09 +0100

    Se añade otro print al archivo

--- proyecto.py
+++ proyecto.py
@@ -3,3 +3,4 @@ from supabase import create_client, Client
 supabase : Client = create_client("https://qpynwhslmdfavlxxpihu.supabase.co"), "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFweW53aHNsbWRmYXZseHhwaWh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzMTk1NzEsImV4cCI6MjAxMTg5NTU3MX0.JKpztUNwvsJDuDpaYoUYOedFHqpkdOJVxNOaalY4KnM"
 print(supabase.table ("Alumnos").select("*").eq("ID,"2,0)execute())
 print("hola")
+print("que tal")
